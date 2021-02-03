5363bfa0600dbaf646a5d0fec3db16ab477c2caa
Mishrilal <mishrilalchhaparia@gmail.com>
2021-02-03 10:12:11 +0530

    Base Setup

--- viewCustomer.php
+++ viewCustomer.php
@@ -9,15 +9,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/navbar.css">
-        <title>Basic Banking</title>
+        <title>View Customers</title>
     </head>
 
     <body>
-        <h3>Welcome to GRIP Bank</h3>
-        <br>
-        <a href="viewCustomer.php"><button>View All Customer</button></a>
-        <a href="makeTransaction.php"><button>Transfer Money</button></a>
-        <a href="transactionHistory.php"><button>Transaction History</button></a>
+        <h3>All Customer Apperars here</h3>
     </body>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
